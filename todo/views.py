65f7838dfe83c3f0c63dd81f6bc8e3319c2065ba
Adedara Olumayowa <adedaramayowa6@gmail.com>
2023-06-20 22:26:53 +0800

    third commit

--- todo/views.py
+++ todo/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 def about(request):
-    return render(request, 'todo/about.html')
+    return render(request, 'todo/templates/about.html')
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'task'
